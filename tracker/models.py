fc34c6081f8749af1b5eda828c099cb57b23513c
Waddles <rowan.belenky@gmail.com>
2026-03-28 18:07:43 -0700

    Added more blank field capability

--- tracker/models.py
+++ tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class CD(models.Model):
-    band = models.CharField(max_length=128)
+    band = models.CharField(max_length=128,  blank=True)
     album = models.CharField(max_length=128, blank=True)
     #other = models.CharField(max_length=512, null=True)
 
