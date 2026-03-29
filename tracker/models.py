6aa54d1693b0182f50aac623c19a19e84b8f3da6
Waddles <rowan.belenky@gmail.com>
2026-03-28 17:36:12 -0700

    update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class CD(models.Model):
     band = models.CharField(max_length=128)
-    album = models.CharField(max_length=128)
+    album = models.CharField(max_length=128, blank=True)
     #other = models.CharField(max_length=512, null=True)
 
     class Meta:
